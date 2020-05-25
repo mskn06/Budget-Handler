6852d1acbc549028e1f6ac3e6070e78cdab03719
Muskaan Jain <muskaan24mj@gmail.com>
2020-05-25 15:55:04 +0530

    set up add-order page

--- frontend/src/app/pages/add-new-order/add-new-order.page.ts
+++ frontend/src/app/pages/add-new-order/add-new-order.page.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-new-order.page.scss'],
 })
 export class AddNewOrderPage implements OnInit {
-  pageName = "add-new-order";
 
   constructor() { }
 
