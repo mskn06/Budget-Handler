404d59fa194193f5597950fb6f94506761cf47c5
Muskaan Jain <muskaan24mj@gmail.com>
2020-05-25 15:19:27 +0530

    sotted setting page

--- frontend/src/app/shared/components/input-row/input-row.component.ts
+++ frontend/src/app/shared/components/input-row/input-row.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-input-row',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputRowComponent implements OnInit {
 
+  @Input() placeholder;
   constructor() { }
 
   ngOnInit() {}
