590e61b62f217c998de8dcd7ab9406baae58b4fe
Muskaan Jain <muskaan24mj@gmail.com>
2020-05-28 19:27:10 +0530

    updated db struct

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,10 +1,17 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
-mongoose.connect(
-  `mongodb+srv://${"budgethandler"}:${"MD5qmOlqAXFWKfWe"}@project-ppgo7.mongodb.net/${"Budget"}?retryWrites=true&w=majority`,
-  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false },
-  (err) => {
-    if (err) console.log("mongo error" + err);
-    else console.log("Db connected...");
+class Connection {
+  constructor() {
+    const url =
+      process.env.MONGODB_URI ||  `mongodb+srv://${"budgethandler"}:${"MD5qmOlqAXFWKfWe"}@project-ppgo7.mongodb.net/${"Budget"}?retryWrites=true&w=majority`;
+    console.log("Established new db connection");
+    mongoose.Promise = global.Promise;
+    mongoose.set("useNewUrlParser", true);
+    mongoose.set("useFindAndModify", false);
+    mongoose.set("useCreateIndex", true);
+    mongoose.set("useUnifiedTopology", true);
+    mongoose.connect(url);
   }
-);
+}
+
+export default new Connection();
