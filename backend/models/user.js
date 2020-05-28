f349bb92403d8bad67c53ad68cb8d3748b2b735c
Muskaan Jain <muskaan24mj@gmail.com>
2020-05-28 19:26:43 +0530

    updated model structure

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,51 +1,60 @@
-const mongoose = require("mongoose");
+import mongoose, { Schema } from "mongoose";
+import uniqueValidator from "mongoose-unique-validator";
 
-const userModel = mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-    },
-    clearanceTime: {
-      type: Number,
-      default: 21,
-    },
-    numberOfOrders: {
-      type: Number,
-      default: 0,
-    },
-    amtPaid: {
-      type: Number,
-      default: 0,
-    },
-    amtCleared: {
-      type: Number,
-      default: 0,
-    },
-    amtInClearance: {
-      type: Number,
-      default: 0,
-    },
-    amtEarned: {
-      type: Number,
-      default: 0,
-    },
-    amtUsed: {
-      type: Number,
-      default: 0,
-    },
-  },
-  {
-    timestamps: true,
+class User {
+
+  initSchema() {
+    const schema = new Schema({
+      name: {
+        type: String,
+        required: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+      },
+      clearanceTime: {
+        type: Number,
+        default: 21,
+      },
+      numberOfOrders: {
+        type: Number,
+        default: 0,
+      },
+      amtPaid: {
+        type: Number,
+        default: 0,
+      },
+      amtCleared: {
+        type: Number,
+        default: 0,
+      },
+      amtInClearance: {
+        type: Number,
+        default: 0,
+      },
+      amtEarned: {
+        type: Number,
+        default: 0,
+      },
+      amtUsed: {
+        type: Number,
+        default: 0,
+      },
+    }, { timestamps: true });
+   
+    schema.plugin(uniqueValidator);
+    mongoose.model("User", schema);
   }
-);
 
-module.exports = mongoose.model("User", userModel);
+  getInstance() {
+    this.initSchema();
+    return mongoose.model("User");
+  }
+}
+
+export default User;
