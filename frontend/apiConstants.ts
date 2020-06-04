5a0bc9676949e38378c2ee0bc8c992ba840a38f8
Muskaan Jain <muskaan24mj@gmail.com>
2020-06-04 23:55:05 +0530

    added APIs

--- frontend/apiConstants.ts
+++ frontend/apiConstants.ts
@@ -3,5 +3,9 @@ const api = "http://localhost:3000";
 export const CONSTANTS = {
   SIGNUP: `${api}/signup`,
   LOGIN: `${api}/login`,
+  UPDATEUSER: `${api}/user/:userId`,
+  ADDSTAFF: `${api}/staff`,
+  GETSTAFFS: `${api}/staff`,
+  ADDPROJECT: `${api}/project`,
   GETPROJECTS: `${api}/project`,
 };
